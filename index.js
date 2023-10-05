4a241685b30ac8b269eae18702ecca820321b950
Анита <anita@Air-Anita.Dlink>
2023-10-05 16:23:17 +0300

    good

--- index.js
+++ index.js
@@ -6,27 +6,29 @@ let secondNum = []
 let sign = null //
 const numbers = [0,1,2,3,4,5,6,7,8,9] 
 const znaki = ["×", '-', '+','÷']
+const tochka = '.'
 let pastSign = null
-document.onclick = function (event) {
+function test (event) {
     if(!event.target.classList.contains('btn')) {
         return
     } 
-
     // console.log(event.target.innerHTML)
-    const znak = event.target.innerHTML 
+    const znak = event.target.innerHTML
     
-  
     if(znaki.includes(znak)){
         if(firstNum.length){
             sign = znak
+            if (firstNum[firstNum.length - 1] === '.') {
+                firstNum.push(0)
+            }
             otvet.innerHTML = firstNum.join('') + sign + secondNum.join('')
         }
       
-    } else if (numbers.includes(Number(znak)) || znak === '.'){
-        if (!sign){
-            firstNum.push(znak)
-        } else {
+    }  else if (numbers.includes(Number(znak))){
+        if (sign){
             secondNum.push(znak)
+        } else {
+            firstNum.push(znak)
         }
         
         otvet.innerHTML = firstNum.join('') + (sign || '') + secondNum.join('')
@@ -49,8 +51,18 @@ document.onclick = function (event) {
         sign = null
         otvet.innerHTML = 'otvet'
         
+    } else if (znak === tochka) {
+        if(sign){
+            if (secondNum[0] && secondNum.indexOf(tochka) < 0){
+                secondNum.push(znak)
+            }
+        } else {
+            if (firstNum[0] && firstNum.indexOf(tochka) < 0){
+                firstNum.push(znak)  
+            }
+        }
+        otvet.innerHTML = firstNum.join('') + (sign || '') + secondNum.join('')
     }
-
 }
 
-
+document.onclick = (e) => test(e)
